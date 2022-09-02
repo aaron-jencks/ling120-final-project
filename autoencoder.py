297a04913b45603e2444f9ba17fcc432bd092511
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2022-09-02 18:36:42 -0500

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 import argparse
 
 from src import settings
-from src.models.dataset import AudioEncoderDataset, find_largest_waveform_size
+from src.models.dataset import AudioEncoderPhonemeDataset, find_largest_waveform_size
 from src.models.generic_model import GeneralPerceptron, train_loop, test_loop
 
 
@@ -18,9 +18,8 @@ if __name__ == '__main__':
     parser.add_argument('clip_dir', type=pathlib.Path, help='The location of the .wav files')
     parser.add_argument('phoneme_dir', type=pathlib.Path,
                         help='The location of the subdirectories of the phoneme clips')
-    parser.add_argument('--layer_count', type=int, default=1, help='Number of layers to use in the MLP')
-    parser.add_argument('--layer_size', type=int, default=3000,
-                        help='Number of neurons in the layers to use in the MLP')
+    parser.add_argument('--encoding_layer_size', type=int, default=3000,
+                        help='Number of neurons in the compression layer')
     parser.add_argument('--learning_rate', type=float, default=0.01, help='The learning rate')
     parser.add_argument('--batch_size', type=int, default=32, help='The batch size for the data loader')
     parser.add_argument('--epochs', type=int, default=10, help='The number of epochs for training')
@@ -40,20 +39,17 @@ if __name__ == '__main__':
 
     print('The largest waveform size is {}'.format(max_output_size))
 
-    dataset = AudioEncoderDataset(args.tsv_file, args.clip_dir, args.phoneme_dir, max_output_size)
+    dataset = AudioEncoderPhonemeDataset(args.tsv_file, args.clip_dir, args.phoneme_dir, max_output_size)
 
     # Version 1 (No Gradient Boosting)
+    print('Generating model')
     if not args.output.exists():
         model = GeneralPerceptron(max_output_size, max_output_size,
-                                  args.layer_count,
-                                  [args.layer_size] * args.layer_count,
+                                  1, [args.encoding_layer_size],
                                   False).to(device)
     else:
         model = torch.load(args.output, map_location=device)['model']
 
-    # Version 2 (Gradient Boosting)
-    # model = GradientBoostingClassifier(model, 10, cuda=torch.cuda.is_available())
-    # model.set_optimizer('SGD', lr=0.0001)
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate)
 
@@ -61,7 +57,7 @@ if __name__ == '__main__':
                       model, criterion, optimizer)
     i = 0
     while abs(loss) > 10 and i < args.epochs:
-        print('{}x{}: Training iteration {}, Loss {}\n'.format(args.layer_count, args.layer_size, i, loss))
+        print('1x{}: Training iteration {}, Loss {}\n'.format(args.encoding_layer_size, i, loss))
         loss = train_loop(torch.utils.data.DataLoader(dataset, batch_size=args.batch_size),  # pin_memory=True),
                           model, criterion, optimizer)
         print('Training Error: {}'.format(loss))
