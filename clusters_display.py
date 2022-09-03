470e38068a94a945f400dfb2e6978e6ae0f662c3
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2022-09-02 19:32:24 -0500

    Update clusters_display.py

--- clusters_display.py
+++ clusters_display.py
@@ -1,10 +1,16 @@
 import pathlib
 import argparse
 
+from sklearn.cluster import KMeans
+from tqdm import tqdm
+
+from src.utils.tsv import read_tsv
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='KNN display for the dataset to analyze phones')
     parser.add_argument('tsv_file', type=pathlib.Path, help='The TSV file to train from')
+    parser.add_argument('--num_clusters', type=int, default=100)
 
     args = parser.parse_args()
     tsv_columns = [
@@ -14,3 +20,9 @@ if __name__ == '__main__':
         'fft',
         'distance'
     ]
+
+    data = read_tsv(args.tsv_file)
+
+    X = [x['distance'] for x in tqdm(data, desc='Getting fft data')]
+
+    model = KMeans(args.num_clusters).fit(X)
